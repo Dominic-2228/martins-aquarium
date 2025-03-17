7f612af23553ee840f316ff57cfeed318dd16d96
Dominic Tillison <22tilldj@gmail.com>
2025-03-17 11:00:55 -0400

    Added CSS Layout

--- scripts/locationList.js
+++ scripts/locationList.js
@@ -1,12 +1,12 @@
 import { database } from "./aquarimData.js"
 const {locations} = database
 export const locationList = () => {
-    let locationHTML = "<h1>Locations</h1>"
+    let locationHTML = '<h1 class="location">Locations</h1>'
   
     for (const list of locations) {
       locationHTML += `
       <div class="locationHTML">
-      <h3>${list.name}</h3>
+      <h3 id="h3location">${list.name}</h3>
       <p>${list.country}</p>
       <p>${list.description}</p>
       </div>
